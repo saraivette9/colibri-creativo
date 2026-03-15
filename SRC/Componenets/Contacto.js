d4ba01e9524701a16afb426d3c070ae76288a92a
saraivette9 <saraivette9@gmail.com>
2026-03-15 15:53:42 -0600

    Add files via upload

---
+++ SRC/Componenets/Contacto.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Contacto() {
+  return (
+    <div>
+      <h2>Contacto</h2>
+
+      <p>Correo: info@colibricreativo.com</p>
+      <p>Teléfono: 555-123-4567</p>
+
+      <h3>Síguenos</h3>
+
+      <p>Facebook</p>
+      <p>Instagram</p>
+      <p>YouTube</p>
+
+    </div>
+  );
+}
+
+export default Contacto;
---
+++ SRC/Componenets/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Desarrollado por Sara Ramos</p>
+      <p>© 2026 Colibrí Creativo</p>
+    </footer>
+  );
+}
+
+export default Footer;
