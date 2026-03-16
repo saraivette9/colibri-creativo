9603ba7d37faa3491efefdb2df76d695f5dd6f69
saraivette9 <saraivette9@gmail.com>
2026-03-15 20:02:41 -0600

    Add files via upload

---
+++ componentes/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Navbar(){
+  return(
+    <nav>
+      <h2>Colibrí Creativo</h2>
+      <ul>
+        <li>Inicio</li>
+        <li>Catálogo</li>
+        <li>Personalización</li>
+        <li>Contacto</li>
+        <li>Carrito</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
